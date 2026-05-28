6af65cfe35b32947ba2cf4a708534bbdaa7da8fc
Ritu23456 <166889303+Ritu23456@users.noreply.github.com>
2026-05-28 14:18:50 +0530

    fix: add CORS allowed origins for Netlify

--- backend/breathe/settings.py
+++ backend/breathe/settings.py
@@ -50,6 +50,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = False
-CORS_ALLOWED_ORIGINS = os.environ.get('CORS_ALLOWED_ORIGINS', 'http://localhost:3000').split(',')
+CORS_ALLOWED_ORIGINS = os.environ.get('CORS_ALLOWED_ORIGINS', 'http://localhost:3000,https://genuine-zabaione-809c05.netlify.app').split(',')
 CORS_ALLOW_CREDENTIALS = True
 REST_FRAMEWORK = {'DEFAULT_RENDERER_CLASSES': ['rest_framework.renderers.JSONRenderer']}
